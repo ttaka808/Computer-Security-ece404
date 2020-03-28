dfc1ac483c3667c1e3b6643326f8c0d7b2523be7
dummy <dummy@dummy.edu>
2020-03-28 16:19:15 -0400

    not sure about ssh porting, but I'm submiting

--- hmwk9/firewall404.sh
+++ hmwk9/firewall404.sh
@@ -11,14 +11,16 @@ sudo iptables -t raw -F
 sudo iptables -t raw -X
 
 # Changing source IP address of all outgoing packets to my own IP
-sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 # Blocking a list of IP addresses
-sudo iptables -A INPUT -s 127.0.0.1 -j DROP
+# [confirmed working]
+sudo iptables -A INPUT -s 126.1.1.25 -j DROP
 sudo iptables -A INPUT -s 52.242.211.89 -j DROP
 sudo iptables -A INPUT -s 10.0.0.165 -j DROP
 
 # Blocking my computer from being pinged by other hosts
+# [confirmed working]
 sudo iptables -A INPUT -p icmp --icmp-type echo-request -j REJECT
 
 # Port forwarding from port 9,000 to port 22 (for tcp or udp)
@@ -28,12 +30,15 @@ sudo iptables -t nat -A PREROUTING -i eth0 -p udp --dport 9000 -j REDIRECT --to-
 
 # Only allowing SSH access only to machines in the engineering.purdue.edu domain
 # blocking all connections first, then allowing those from ^
+# [confirmed working]
 sudo iptables -A INPUT -p tcp --dport ssh -j DROP
 sudo iptables -A INPUT -p tcp --dport ssh -s 128.46.104.5 -j ACCEPT
 
 # Rule for only allowing a single IP address on the internet to access machine
+# [confirmed working]
 sudo iptables -A INPUT -p tcp --dport http -j DROP
 sudo iptables -A INPUT -p tcp --dport http -s 128.46.104.5 -j ACCEPT
 
 # Permit Auth/Ident (port 113) that's used by services like SMTP and IRC
+# [confirmed working]
 sudo iptables -A INPUT -p tcp --dport 113 -j ACCEPT
